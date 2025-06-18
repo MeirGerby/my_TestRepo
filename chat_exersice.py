7354586c93f6f3b011e45bc6a20218bf4a9ece8f
MeirGerby <meirg2001@gmail,com>
2025-06-18 23:04:27 +0300

    new version

---
+++ chat_exersice.py
@@ -0,0 +1,66 @@
+person = {
+    "name": "Meir",
+    "age": 25,
+    "city": "Jerusalem"
+}
+
+print(person["city"])
+person["job"] = "Programmer"
+person["age"] = 26
+
+print(person)
+for key in person:
+    print(key, ":", person[key])
+
+for key, value in person.items():
+    print(f"{key} -> {value}")
+
+
+sara = {
+    "name" : "Sara",
+    "age" : 29,
+    "city" : "Boston",
+    "hobby": "bake"
+}
+
+print(sara["name"])
+sara["age"] = 30
+sara["unit"] = "cyber"
+
+
+
+
+moshe = {
+    "name" : "Moshe",
+    "age" : 24,
+    "city" : "Haifa",
+    "unit" : "None"
+}
+
+ron = {
+    "name" : "Ron",
+    "age" : 24,
+    "city" : "Jerusalem",
+    "unit" : "Programming"
+}
+
+david = {
+    "name" : "David",
+    "age" : 27,
+    "city" : "Bene Brak",
+    "unit" : "cyber"
+}
+
+names = [moshe, ron, david]
+
+
+for person in names:
+    print(f"{person["name"]} from {person["city"]} is {person["age"]} years old and serves in {person["unit"]}")
+
+
+
+
+
+
+
+
